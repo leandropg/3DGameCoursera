a9417f9e184c7a51eb3768ad6538de2793238f73
Leandro Perez <leandropg@gmail.com>
2016-08-13 08:13:44 -0500

    Funcionamiento hasta que llega al carro de juguete

---
+++ Assets/Scripts/lperez/GenerateObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GenerateObject : MonoBehaviour {
+
+    /**
+     * Game Object
+     */ 
+    public GameObject obj;
+
+    /**
+     * Start Delay
+     */ 
+    public float startDelay = 0.0f;
+
+    /**
+     * Delay Between Generate
+     */ 
+    public float betweenDelay = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        InvokeRepeating("ObjectGenerator", startDelay, betweenDelay);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /**
+     * Generate Object
+     */
+    void ObjectGenerator() {
+
+        // Instance Game Object
+        Instantiate(obj);
+
+        // Put Object into position
+        obj.transform.position = transform.position;
+    }
+}
