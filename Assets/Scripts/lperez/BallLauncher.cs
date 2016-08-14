fd3e89b24a352eb84f65e81078cb451578b80bcc
Leandro Perez <leandropg@gmail.com>
2016-08-13 20:32:10 -0500

    Adicion del switch de la maquina de Goldberg

---
+++ Assets/Scripts/lperez/BallLauncher.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+
+/**
+ * Ball Launcher
+ */
+public class BallLauncher : MonoBehaviour {
+
+    /**
+     * Ball Prefab
+     */
+    public GameObject ballPrefab;
+
+    /**
+     * Start Delay
+     */
+    public float startDelay = 0.0f;
+
+    /**
+     * Delay Between Generate
+     */
+    public float betweenDelay = 10.0f;
+
+    /**
+     * Force X
+     */
+    public float forceX = 0.0f;
+
+    /**
+     * Force Y
+     */
+    public float forceY = 0.0f;
+
+    /**
+     * Force Z
+     */
+    public float forceZ = 0.0f;
+
+    /**
+     * Flag Is Started
+     */
+    private bool isStarted = false;
+
+    /*
+     * Start Method
+     */
+    void Start () {
+
+        GameObject startLigth;
+        Light ligth;
+
+        // Get Start Light
+        startLigth = GameObject.Find("Start Light");
+        ligth = startLigth.GetComponentInChildren<Light>();
+        ligth.enabled = false;
+    }
+	
+	/**
+     * Update Method
+     */
+	void Update () {
+	
+	}
+
+    /**
+     * On Enable
+     */ 
+    void OnEnable() {
+
+        ControlPostBox.OnStartMachine += StartStopBallLauncher;
+    }
+
+    /**
+     * On Disable
+     */
+    void OnDisable() {
+
+        ControlPostBox.OnStartMachine += StartStopBallLauncher;
+    }
+
+    /**
+     * Start Ball Launcher
+     */
+    void StartStopBallLauncher() {
+
+        GameObject startLigth;
+        Light ligth;
+
+        // Get Start Light
+        startLigth = GameObject.Find("Start Light");
+        ligth = startLigth.GetComponentInChildren<Light>();
+
+        // Check Flag Is Started
+        if (!isStarted) {
+
+            InvokeRepeating("LaunchBall", startDelay, betweenDelay);
+            isStarted = true;
+            ligth.enabled = true;
+
+        } else {
+
+            CancelInvoke();
+            isStarted = false;
+            ligth.enabled = false;
+        }
+    }
+
+    /**
+     * Launch Ball
+     */
+    void LaunchBall()
+    {
+        GameObject ball;
+        Rigidbody rgb;
+
+        // Instance Ball
+        ball = Instantiate(ballPrefab);
+
+        // Put Ball in Initial Position
+        ball.transform.position = transform.position;
+
+        // Get Ball Rigid Body
+        rgb = ball.GetComponent<Rigidbody>();
+
+        // Apply Force to Ball
+        rgb.AddRelativeForce(new Vector3(forceX, forceY, forceZ), ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/lperez/ControlPostBox.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+/**
+ * Control Post Box
+ */
+public class ControlPostBox : MonoBehaviour {
+
+    /**
+     * Animator Instance
+     */
+    private Animator anim;
+
+    /**
+     * Delegate Start Machine
+     */ 
+    public delegate void StartMachine();
+
+    /**
+     * Event On Start Machine
+     */ 
+    public static event StartMachine OnStartMachine;
+
+    /**
+     * Start Method
+     */
+    void Start () {
+
+        // Get Instances
+        anim = GetComponent<Animator>();
+    }
+
+    /**
+     * Update Method
+     */
+    void Update () {
+	
+	}
+
+    /**
+     * On Trigger Enter
+     */ 
+    void OnTriggerEnter(Collider collider) {
+
+        // Check if Ethan Collision
+        if (collider.gameObject.name.Contains("Ethan")) {
+
+            // Check if Event not is null
+            if(OnStartMachine != null) {
+
+                OnStartMachine();
+            }
+        }
+    }
+}
