567f701eb0a82404295ce9c1ad10c42aa7788de5
Leandro Perez <leandropg@gmail.com>
2016-08-14 12:57:32 -0500

    Ajuste del sistema de particulas que simulan el agua

---
+++ Assets/Scripts/lperez/ControlHydrant.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+/**
+ * Control Hydrant
+ */ 
+public class ControlHydrant : MonoBehaviour {
+
+    /**
+     * Delegate Open Water
+     */
+    public delegate void OpenWater();
+
+    /**
+     * Event On Open Water
+     */
+    public static event OpenWater OnOpenWater;
+
+    /**
+     * Start Method
+     */
+    void Start () {
+	
+	}
+	
+	/**
+     * Update Method
+     */ 
+	void Update () {
+	
+	}
+
+    /**
+     * On Trigger Enter
+     */
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject hydrantWater;
+        ParticleSystem particleSystem;
+
+        // Check if Ethan Collision
+        if (collider.gameObject.name.Contains("Bus"))
+        {
+            // Get Hydrant Water
+            hydrantWater = GameObject.Find("HydrantWater");
+
+            // Get Particle System
+            particleSystem = hydrantWater.GetComponentInChildren<ParticleSystem>();
+
+            // Play Animation
+            particleSystem.Play();
+
+            // Check if Event not is null
+            if (OnOpenWater != null) {
+
+                OnOpenWater();
+            }
+        }
+    }
+}
